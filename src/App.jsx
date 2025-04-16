b4dac9be7f63eb0946d576ea38d11e4c9371cd05
ashish <ashishmangla1991@gmail.com>
2025-04-16 17:48:25 +0530

    add new code

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,17 @@
-import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import {
-  decrement,
-  increment,
-  incremetByAmount,
-  reset,
-} from "./features/counter/counterSlice";
 import HomePage from "./pages/HomePage";
 import { Route, Routes } from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import Header from "./components/Header";
 
 function App() {
-  const count = useSelector((state) => state?.counter?.value);
-
-  const dispatch = useDispatch();
-
-  const handleIncrement = () => {
-    dispatch(increment());
-  };
-
-  const handleDecrement = () => {
-    dispatch(decrement());
-  };
-  const handleReset = () => {
-    dispatch(reset());
-  };
-  const handleIncrementByAmount = (e) => {
-    const value = e.target.textContent;
-
-    dispatch(incremetByAmount(parseInt(value)));
-  };
-
   return (
     <>
-   <Header/>
-     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/about" element={<AboutPage />} />
-      {/* <Route path="/contact" element={<ContactPage />} />
-      <Route path="*" element={<NotFoundPage />} />  */}
-    </Routes>
-    
-      {/* <div>
-        {count}
-        <button onClick={handleIncrement}>+</button>
-        <button onClick={handleDecrement} disabled={count <= 0}>
-          -
-        </button>
-        <button onClick={handleReset}>reset</button>
-        <button onClick={handleIncrementByAmount}>5</button>
-      </div> */}
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
     </>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,23 +1,44 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrement,
+  increment,
+  incremetByAmount,
+  reset,
+} from "../features/counter/counterSlice";
 
-const HomePage = () =>{
+const HomePage = () => {
+  const count = useSelector((state) => state?.counter?.value);
+  const dispatch = useDispatch();
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+  const handleReset = () => {
+    dispatch(reset());
+  };
 
-    const count =  useSelector(state => state?.counter?.value)
-return(
+  const handleIncrementByAmount = (e) => {
+    const value = e?.target?.textContent;
+
+    dispatch(incremetByAmount(parseInt(value)));
+  };
+
+  return (
     <>
-    <div className="counter_main">
+      <div className="counter_main">
         {count}
 
-
         <div className="">
-            <button>+</button>
-            <button>-</button>
-            <button>Reset</button>
-            <button>5</button>
+          <button onClick={handleIncrement}>+</button>
+          <button onClick={handleDecrement}>-</button>
+          <button onClick={handleReset}>Reset</button>
+          <button onClick={handleIncrementByAmount}>5</button>
         </div>
-    </div>
+      </div>
     </>
-)
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
